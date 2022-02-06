b739e1ff4a2f32d40c93425051bf2fe4760b471b
Isidro Arias <isidroariass@hotmail.es>
2022-02-06 16:47:45 +0100

    relative image path

--- neopixel_emulator.py
+++ neopixel_emulator.py
@@ -1,5 +1,7 @@
 import pyglet
 import math
+import os
+from pathlib import Path
 
 class NeoPixel_Emulator(pyglet.window.Window):
     def __init__(self, window_w=1765, window_h=400):
@@ -10,20 +12,22 @@ class NeoPixel_Emulator(pyglet.window.Window):
         self.led_group = pyglet.graphics.OrderedGroup(0)
         self.color_group = pyglet.graphics.OrderedGroup(1)
         self.alive = 1
+        self.image_path = Path(__file__).parent / 'ws2812b.png'
+        self.image_path2 = Path(__file__).parent / 'circle.png'
     def draw_LEDs(self, led_number):
         for led in range(led_number):
-            img = pyglet.image.load('ws2812b.png')
+            img = pyglet.image.load(self.image_path)
             self.sprites.append(pyglet.sprite.Sprite(img=img,batch=self.batch, x=(led-50*(led//50))*35, y=self.height-34-((led//50)*34), group=self.led_group))
-            self.color_sprites.append(pyglet.sprite.Sprite(img=pyglet.image.load('circle.png'),batch=self.batch,x=(led-50*(led//50))*35,y=self.height-34-((led//50)*34)-5,group=self.color_group))
+            self.color_sprites.append(pyglet.sprite.Sprite(img=pyglet.image.load(self.image_path2),batch=self.batch,x=(led-50*(led//50))*35,y=self.height-34-((led//50)*34)-5,group=self.color_group))
             self.sprites[led].scale = .1
             self.color_sprites[led].color = (0,0,0)
             #print(self.sprites[led].position)
     def draw_LED_matrix(self, width, height):
         for y in range(height):
             for x in range(width):
-                img = pyglet.image.load('ws2812b.png')
+                img = pyglet.image.load(self.image_path)
                 self.sprites.append(pyglet.sprite.Sprite(img=img,batch=self.batch, x=x*35, y=self.height-34-(y*34), group=self.led_group))
-                self.color_sprites.append(pyglet.sprite.Sprite(img=pyglet.image.load('circle.png'),batch=self.batch,x=x*35,y=self.height-34-(y*34)-5,group=self.color_group))
+                self.color_sprites.append(pyglet.sprite.Sprite(img=pyglet.image.load(self.image_path2),batch=self.batch,x=x*35,y=self.height-34-(y*34)-5,group=self.color_group))
                 self.sprites[y*width+x].scale = .1
                 self.color_sprites[y*width+x].color = (0,0,0)
     def map(self,input_val,in_min,in_max,out_min,out_max):
